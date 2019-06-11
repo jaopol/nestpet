b118dc45eb74fa0a096a275814eb02aeac0e3a9e
jaopol <jaopol@gmail.com>
2019-06-10 23:12:10 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/app/nestpet/api/NestpetApplication.java
@@ -0,0 +1,13 @@
+package com.app.nestpet.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestpetApplication.class, args);
+	}
+
+}
